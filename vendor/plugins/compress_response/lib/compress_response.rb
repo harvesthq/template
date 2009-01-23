1c7dccc6d1ec14311b567c812ee4cced494f01d9
Barry Hess <barry@bjhess.com>
2009-01-23 10:50:04 -0600

    Add compress_response plugin.

---
+++ vendor/plugins/compress_response/lib/compress_response.rb
@@ -0,0 +1,30 @@
+require 'compress_request/request_dont_compress'
+
+module CompressResponse
+
+  protected
+
+  # based on AWDR, file 140
+  def compress
+    return if request.skip_compression?
+    return if response.body.length < 2.kilobytes
+    accepts = request.env['HTTP_ACCEPT_ENCODING']
+    return unless accepts && accepts =~ /(x-gzip|gzip)/
+    encoding = $1
+    compressed = ActiveSupport::Gzip.compress(response.body)
+    if compressed.length < response.body.length
+      response.body = compressed
+      response.headers['Content-Encoding'] = encoding
+    end
+  end
+
+end
+
+module ActionController
+  class AbstractRequest
+    attr_writer :dont_compress
+    def skip_compression?
+      @dont_compress
+    end
+  end
+end
